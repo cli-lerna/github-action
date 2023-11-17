93129af51a76464e46b8f6a9e05a161a874be3b2
lumeiqin <meiqinlu@163.com>
2023-11-17 23:34:52 +0800

    测试同步问题

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,17 @@
  * @Author: lumeiqin meiqinlu@163.com
  * @Date: 2023-11-01 13:58:26
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2023-11-17 11:04:27
+ * @LastEditTime: 2023-11-17 23:34:03
  * @FilePath: /study1102/template/vue-template/template/src/App.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <template>
     <div>
         <div>测试发布啦啦啦啦</div>
-    <h2>哈哈哈哈哈</h2>
+        <h2>哈哈哈哈哈</h2>
+    嘻嘻
+
+    学习格式化vue文件
     </div>
 </template>
 

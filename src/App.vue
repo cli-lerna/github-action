b42d3c951d73bc7c0f06772f756252a747a14255
lumeiqin <meiqinlu@163.com>
2023-11-17 23:44:58 +0800

    jenkins

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,3 @@
-<!--
- * @Author: lumeiqin meiqinlu@163.com
- * @Date: 2023-11-01 13:58:26
- * @LastEditors: Please set LastEditors
- * @LastEditTime: 2023-11-17 23:34:03
- * @FilePath: /study1102/template/vue-template/template/src/App.vue
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
--->
 <template>
     <div>
         <div>测试发布啦啦啦啦</div>

11fd6ed51e369c46a239e53b6966e40f01e4ddee
lumeiqin <meiqinlu@163.com>
2023-11-17 11:04:44 +0800

    测试发布1

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,15 @@
  * @Author: lumeiqin meiqinlu@163.com
  * @Date: 2023-11-01 13:58:26
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2023-11-17 10:58:24
+ * @LastEditTime: 2023-11-17 11:04:27
  * @FilePath: /study1102/template/vue-template/template/src/App.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <template>
-    <div>测试发布啦啦啦啦</div>
+    <div>
+        <div>测试发布啦啦啦啦</div>
+    <h2>哈哈哈哈哈</h2>
+    </div>
 </template>
 
 <script setup>
